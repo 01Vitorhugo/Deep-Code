098d9fc4acbfe8a49f2cdb419d1fe40ae81213aa
Vitor Hugo <vitorhugo0031@outlook.com>
2022-10-28 17:18:40 -0300

    estruturando pagina

---
+++ script.js
@@ -0,0 +1,19 @@
+
+window.sr = ScrollReveal({ reset: true});
+
+
+sr.reveal('.foto-logo-txt', {duration: 3000});
+sr.reveal('.quemSomos-rodape', {duration: 2000});
+sr.reveal('.topo-OqueFazemos', {duration: 1000});
+
+
+
+
+sr.reveal('.OqueFazemos-box1', {duration: 2000});
+sr.reveal('.OqueFazemos-box2', {duration: 2000});
+sr.reveal('.OqueFazemos-box3', {duration: 1000});
+sr.reveal('.OqueFazemos-box4', {duration: 1000});
+
+
+
+
