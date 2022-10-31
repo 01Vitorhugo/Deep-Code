bc06fe43aca17f029f2c8635947c5f3a89c7f837
Vitor Hugo <vitorhugo0031@outlook.com>
2022-10-31 17:13:18 -0300

    ajustando pagina O que fazemos

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ sr.reveal('.topo-OqueFazemos', {duration: 1000});
 
 sr.reveal('.OqueFazemos-box1', {duration: 2000});
 sr.reveal('.OqueFazemos-box2', {duration: 2000});
-sr.reveal('.OqueFazemos-box3', {duration: 1000});
-sr.reveal('.OqueFazemos-box4', {duration: 1000});
+sr.reveal('.OqueFazemos-box3', {duration: 2000});
+sr.reveal('.OqueFazemos-box4', {duration: 2000});
 
 
 
