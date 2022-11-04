f586f08de60942d0976ddbce0d75fefd5c773217
Vitor Hugo <vitorhugo0031@outlook.com>
2022-11-04 17:21:04 -0300

    atualizando codigos e efeito scroll

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ sr.reveal('#OqueFazemos', {duration: 1500 });
 sr.reveal('#OqueFazemos-lista', {duration: 1500});
 
 
+
+
 sr.reveal('#logo-texto', {duration: 1500});
 sr.reveal('#p-logo-texto', {duration: 1500});
 sr.reveal('#contato', {duration: 1500});
 
+
+sr.reveal('#sobre', {duration: 1500});
 
 
 
