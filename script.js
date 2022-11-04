6f5e5f50cb2139bfbf77e1d4cb0d0b026158b55f
Vitor Hugo <vitorhugo0031@outlook.com>
2022-11-03 22:08:32 -0300

    codigos atualizados

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 
-window.sr = ScrollReveal({ reset: true});
+const sr = ScrollReveal({ 
+    reset: true});
+
+sr.reveal('#OqueFazemos', {duration: 1500 });
+sr.reveal('#OqueFazemos-lista', {duration: 1500});
 
 
-sr.reveal('.foto-logo-txt', {duration: 3000});
-sr.reveal('.quemSomos-rodape', {duration: 2000});
-sr.reveal('.topo-OqueFazemos', {duration: 1000});
+sr.reveal('#logo-texto', {duration: 1500});
+sr.reveal('#p-logo-texto', {duration: 1500});
+sr.reveal('#contato', {duration: 1500});
 
 
 
-
-sr.reveal('.OqueFazemos-box1', {duration: 2000});
-sr.reveal('.OqueFazemos-box2', {duration: 2000});
-sr.reveal('.OqueFazemos-box3', {duration: 2000});
-sr.reveal('.OqueFazemos-box4', {duration: 2000});
 
 
 
